08384a4b7fa107e4ecfb85b72857b9900e0f4a36
Seayon <153660639@qq.com>
2022-01-20 23:18:54 +0800

    希尔排序再次练习

--- src/main/java/com/seayon/myleetcode/sort/ShellSort.java
+++ src/main/java/com/seayon/myleetcode/sort/ShellSort.java
@@ -17,19 +17,17 @@ public class ShellSort implements MySort {
         }
         //分组,然后进行插入排序
         //设定步长,初始组数直接设置为数组长度的一半儿,组数也是步长,每一组之间的间隔
-        int step = array.length / 2; // 9 /2 = 4, 10 /2 = 5
-        while (step >= 1) {
-            // 对每一组进行插入排序操作,这里一定是 i++,代表一次对每个组进行插入排序
+        int step = array.length / 2;
+        while (step > 0) {
             for (int i = step; i < array.length; i++) {
-                int current = array[i];
-                int j = i - step;
-                while (j >= 0 && current < array[j]) {
-                    array[j + step] = array[j];
+                int j = i;
+                int current = array[j];
+                while (j > step - 1 && current < array[j - step]) {
+                    array[j] = array[j - step];
                     j -= step;
                 }
-                array[j + step] = current;
+                array[j] = current;
             }
-            //步长减半,最后一步,step = 1 的时候, 1/2 等于 0,所以上面不会再进行下一轮
             step /= 2;
         }
         return array;
