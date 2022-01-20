2fa756d8d15b951b63372e402f780d2b3d416ece
Seayon <153660639@qq.com>
2022-01-20 16:38:09 +0800

    选择排序

--- src/main/java/com/seayon/myleetcode/sort/SelectSort.java
+++ src/main/java/com/seayon/myleetcode/sort/SelectSort.java
@@ -7,26 +7,21 @@ package com.seayon.myleetcode.sort;
  * @Author: SaeyonZhao
  * @Date: 2021/12/18 11:44 下午
  * @Version V1.0
- * @Description:
+ * @Description: 选择排序
  */
 public class SelectSort implements MySort {
 
     @Override
     public int[] sort(int[] array) {
-        if (array == null || array.length == 0) {
-            return array;
-        }
-        for (int j = array.length - 1; j > 0; j--) {
-            // 假定第一个位置的数最大
-            int maxIndex = 0;
-            for (int i = 0; i < j + 1; i++) {
-                if (array[i] > array[maxIndex]) {
-                    maxIndex = i;
+        for (int i = 0; i < array.length; i++) {
+            // 每次遍历,从剩下的序列中找到最小元素,放在前面序列的末尾
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
                 }
             }
-            int tmp = array[j];
-            array[j] = array[maxIndex];
-            array[maxIndex] = tmp;
+            swap(array, i, minIndex);
         }
         return array;
     }
