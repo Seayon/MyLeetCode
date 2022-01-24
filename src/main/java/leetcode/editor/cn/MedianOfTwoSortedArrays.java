78191f097ba683013f8413df007731e80de1b8eb
Seayon <153660639@qq.com>
2022-01-24 21:36:40 +0800

    #寻找两个正序数组的中位数 临摹一遍分隔算法

--- src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
+++ src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -72,8 +72,42 @@ public class MedianOfTwoSortedArrays {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            int[] tmp = new int[nums1.length + nums2.length];
 
+
+            return solutionTwo(nums1, nums2);
+        }
+
+        private double solutionTwo(int[] nums1, int[] nums2) {
+            int n = nums1.length;
+            int m = nums2.length;
+            int left = (n + m + 1) / 2;
+            int right = (n + m + 2) / 2;
+            return (findKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + findKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+        }
+
+        public double findKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+            int len1 = end1 - start1 + 1;
+            int len2 = end2 - start2 + 1;
+            if (len1 > len2) {
+                return findKth(nums2, start2, end2, nums1, start1, end1, k);
+            }
+            if (len1 == 0) {
+                return nums2[start2 + k - 1];
+            }
+            if (k == 1) {
+                return Math.min(nums1[start1], nums2[start2]);
+            }
+            int i = start1 + Math.min(len1, k / 2) - 1;
+            int j = start2 + Math.min(len2, k / 2) - 1;
+            if (nums1[i] > nums2[j]) {
+                return findKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+            } else {
+                return findKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+            }
+        }
+
+        private double solutionOne(int[] nums1, int[] nums2) {
+            int[] tmp = new int[nums1.length + nums2.length];
             int i = 0, j = 0, k = 0;
             while (i < nums1.length && j < nums2.length) {
                 if (nums1[i] < nums2[j]) {
