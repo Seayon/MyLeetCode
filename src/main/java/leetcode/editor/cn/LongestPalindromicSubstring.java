871476bad4684736ccf959e0eee3dbeb26b9a151
Seayon <153660639@qq.com>
2022-01-24 23:49:49 +0800

    #5 最长回文子串 艰难的中心扩散方法

--- src/main/java/leetcode/editor/cn/LongestPalindromicSubstring.java
+++ src/main/java/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -1,4 +1,4 @@
-  //给你一个字符串 s，找到 s 中最长的回文子串。
+//给你一个字符串 s，找到 s 中最长的回文子串。
 //
 //
 //
@@ -42,21 +42,52 @@
 // Related Topics 字符串 动态规划 👍 4464 👎 0
 
 
-  package leetcode.editor.cn;
-  public class LongestPalindromicSubstring{
-      public static void main(String[] args) {
-           Solution solution = new LongestPalindromicSubstring().new Solution();
-          // "abccdbeeeeeeeee";
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String longestPalindrome(String s) {
-        if (s.length() == 1) {
-            return s;
-        }
-        return null;
+package leetcode.editor.cn;
+
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new LongestPalindromicSubstring().new Solution();
+        Assertions.assertEquals("bab", solution.longestPalindrome("babad"));
+        Assertions.assertEquals("bbbb", solution.longestPalindrome("bbbbd"));
+        Assertions.assertEquals("bb", solution.longestPalindrome("cbbd"));
+        Assertions.assertEquals("a", solution.longestPalindrome("a"));
+        Assertions.assertEquals("a", solution.longestPalindrome("ac"));
+        // "abccdbeeeeeeeee";
+        Assertions.assertEquals("eeeeeeeee", solution.longestPalindrome("abccdbeeeeeeeee"));
+        Assertions.assertEquals("ac", solution.longestPalindrome("a"));
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestPalindrome(String s) {
+            char[] chars = s.toCharArray();
+            String result = "";
+            for (int i = 0; i < chars.length; i++) {
+                int left = i - 1;
+                int right = i + 1;
+                while (right < chars.length && chars[right] == chars[i]) {
+                    right++;
+                    i = right - 1;
+                }
+                while (left > -1 && chars[left] == chars[i]) {
+                    left--;
+                }
+                while (left > -1 && right < chars.length && chars[left] == chars[right]) {
+                    left--;
+                    right++;
+                }
+                String tmpResultChar = new String(Arrays.copyOfRange(chars, left + 1, right));
+                if (tmpResultChar.length() > result.length()) {
+                    result = tmpResultChar;
+                }
+            }
+            return result;
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
