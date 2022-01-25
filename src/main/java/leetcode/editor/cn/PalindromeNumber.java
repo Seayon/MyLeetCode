3b8076dd5a072486cbce3c3ab7774c04067ce68b
Seayon <153660639@qq.com>
2022-01-25 15:29:11 +0800

    #9判断回文数 遍历各位数字加起来求解

--- src/main/java/leetcode/editor/cn/PalindromeNumber.java
+++ src/main/java/leetcode/editor/cn/PalindromeNumber.java
@@ -55,7 +55,19 @@ import org.junit.jupiter.api.Assertions;
 public class PalindromeNumber {
     public static void main(String[] args) {
         Solution solution = new PalindromeNumber().new Solution();
+
         Assertions.assertEquals(true, solution.isPalindrome(121));
+        Assertions.assertEquals(false, solution.isPalindrome(01210));
+        Assertions.assertEquals(false, solution.isPalindrome(1210));
+        Assertions.assertEquals(false, solution.isPalindrome(01210));
+        Assertions.assertEquals(true, solution.isPalindrome(3412143));
+        Assertions.assertEquals(true, solution.isPalindrome(341020143));
+        Assertions.assertEquals(true, solution.isPalindrome(340020043));
+        Assertions.assertEquals(true, solution.isPalindrome(340000043));
+        Assertions.assertEquals(true, solution.isPalindrome(40022004));
+        Assertions.assertEquals(true, solution.isPalindrome(4002004));
+        Assertions.assertEquals(false, solution.isPalindrome(4002204));
+        Assertions.assertEquals(false, solution.isPalindrome(34102143));
         Assertions.assertEquals(false, solution.isPalindrome(-121));
         Assertions.assertEquals(false, solution.isPalindrome(10));
         Assertions.assertEquals(true, solution.isPalindrome(1));
@@ -66,15 +78,38 @@ public class PalindromeNumber {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean isPalindrome(int x) {
-            char[] chars = String.valueOf(x).toCharArray();
-            boolean isPalindrome = true;
-            for (int i = 0, j = chars.length - 1; i < j; i++, j--) {
-                if (chars[i] != chars[j]) {
-                    isPalindrome = false;
-                    break;
-                }
+            if (x < 0) {
+                return false;
             }
-            return isPalindrome;
+            if (x > 0 && x < 10) {
+                return true;
+            }
+            int o = x;
+            int p = 1;
+            int r = 0;
+            int divid = 1000000000;
+            while (divid > 0) {
+                int quotient = x / divid;
+                if (quotient == 0) {
+                    // 如果当前还没有值
+                    if (r == 0) {
+                        // 就略过
+                        divid /= 10;
+                        continue;
+                    } else {
+                        // 否则就记录下来这个间隙
+                        divid /= 10;
+                        p *= 10;
+                        continue;
+                    }
+                }
+                // 累加结果值
+                r = quotient * p + r;
+                x = x - divid * quotient;
+                divid /= 10;
+                p *= 10;
+            }
+            return r == o;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
